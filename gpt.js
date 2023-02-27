cc3105d94a40867362082c78653a0df276b5c04f
Dylan Martin <dylanrossmartin@gmail.com>
2023-02-26 22:29:20 -0500

    google news

--- gpt.js
+++ gpt.js
@@ -4,27 +4,27 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-async function getAnswer(prompt){
+async function getAnswer(prompt) {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: prompt,
         temperature: 0,
         max_tokens: 50,
-      });
-      const sanitizedJSON = sanitizeJSON(response.data.choices[0].text);
-      return JSON.parse(sanitizedJSON);
+    });
+    const sanitizedJSON = sanitizeJSON(response.data.choices[0].text);
+    return JSON.parse(sanitizedJSON);
 }
 
-function sanitizeJSON(text){
+function sanitizeJSON(text) {
     const startIndex = text.indexOf('{');
     const endIndex = text.lastIndexOf('}');
-    const result = text.substring(startIndex, endIndex+1);
+    const result = text.substring(startIndex, endIndex + 1);
     return result;
 }
 
 // @param queryString: String - the query string that was used to generate the news articles. This string must be a yes or no question.
-function generatePrompt(searchString,question,  newsArticles) {
-    let prompt = `The following results are from newsapi.com when searching for "${searchString}", assuming the latest developments are captured in these headlines, please respond to the question "${question}" with a json object of the following format {"answer": <Boolean>, "confidence": <Number>}. "answer" should be your best guess based on the newsApi results, and "confidence" should be a number between 0 and 1 representing your confidence in your answer. \n\n`;
+function generatePrompt(searchString, question, newsArticles) {
+    let prompt = `The following results are from a google search for "${searchString}", assuming the latest developments are captured in these headlines, please respond to the question "${question}" with a json object of the following format {"answer": <Boolean>, "confidence": <Number>}. "answer" should be your best guess based on the newsApi results, and "confidence" should be a number between 0 and 1 representing your confidence in your answer. \n\n`;
     prompt += JSON.stringify(newsArticles, null, 2);
     return prompt
 }
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const {getNews} = require('./news-api.js');
-const {getAnswer, generatePrompt} = require('./gpt.js');
+const { getAnswer, generatePrompt } = require('./gpt.js');
+const { getGoogleNews } = require('./google.js');
 
 (async (searchString, question) => {
     try {
-        const newsArticles = await getNews(searchString);
+        const newsArticles = await getGoogleNews(searchString);
         const prompt = generatePrompt(searchString, question, newsArticles);
         const answer = await getAnswer(prompt);
         console.log(`based on ${newsArticles.length} news articles, the answer to "${question}" is ${answer.answer} with a confidence of ${answer.confidence}}`)
