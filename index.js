7c806c06535c37cacb942e0f96724f2411c117d5
Dylan Martin <dylanrossmartin@gmail.com>
2023-02-26 23:47:44 -0500

    fix typo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { getGoogleNews } = require('./google.js');
         const newsArticles = await getGoogleNews(searchString);
         const prompt = generatePrompt(searchString, question, newsArticles);
         const answer = await getAnswer(prompt);
-        console.log(`based on ${newsArticles.length} news articles, the answer to "${question}" is ${answer.answer} with a confidence of ${answer.confidence}}`)
+        console.log(`based on ${newsArticles.length} news articles, the answer to "${question}" is ${answer.answer} with a confidence of ${answer.confidence}`)
     } catch (err) {
         console.log(err)
     }
