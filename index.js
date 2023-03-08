dfb876b278fab217b553fdec23cbdd67446e3769
Dylan Martin <dylanrossmartin@gmail.com>
2023-03-08 00:24:28 -0500

    add

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const { getAnswer, generatePrompt } = require('./gpt.js');
-const { getGoogleNews } = require('./google.js');
+const {searchAndDigest} = require('./lambda/search-and-digest.js');
 
 (async (searchString, question) => {
-    try {
-        const newsArticles = await getGoogleNews(searchString);
-        const prompt = generatePrompt(searchString, question, newsArticles);
-        const answer = await getAnswer(prompt);
-        console.log(`based on ${newsArticles.length} news articles, the answer to "${question}" is ${answer.answer} with a confidence of ${answer.confidence}`)
-    } catch (err) {
-        console.log(err)
-    }
+    const result = await searchAndDigest(searchString, question);
+    console.log(result);
 })("jimmy carter", "is jimmy carter alive?")
